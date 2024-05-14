345b80cd9ec687264329efb22c9dcee2b101a981
saadzafar659 <saadzafar659@gmail.com>
2024-05-14 15:09:12 +0200

    integrate cover all

--- src/main/java/tms/task_management_system/TaskManagementSystemApplication.java
+++ src/main/java/tms/task_management_system/TaskManagementSystemApplication.java
@@ -1,4 +1,4 @@
-package tms.Task_Management_System;
+package tms.task_management_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
