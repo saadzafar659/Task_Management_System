6e47635825910204f955774ed907e82856fd6734
saadzafar659 <saadzafar659@gmail.com>
2024-07-16 15:06:34 +0200

    UserWebControllerE2E, TaskWebControllerE2E

---
+++ src/test/java/tms/task_management_system/e2e/TaskWebControllerE2E.java
@@ -0,0 +1,141 @@
+package tms.task_management_system.e2e;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import tms.task_management_system.con.Conn;
+import tms.task_management_system.entity.Task;
+import tms.task_management_system.entity.Users;
+import tms.task_management_system.repository.TaskRepository;
+import tms.task_management_system.repository.UserRepository;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TaskWebControllerE2E extends Conn {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private WebDriver driver;
+    private Users user;
+
+    @BeforeEach
+    @Transactional
+    public void setUp() {        
+        WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		driver = new ChromeDriver(options);
+        
+
+        taskRepository.deleteAll();
+        userRepository.deleteAll();
+
+        user = new Users(1L, "Saad Zafar", "saad@gmail.com", "password1", "ADMIN", null);
+        user = userRepository.save(user);
+
+        Task task1 = new Task(1L, "Title1", "Description1", "2024-5-31", "Pending", user);
+        Task task2 = new Task(2L, "Title2", "Description2", "2024-5-31", "Completed", user);
+        List<Task> taskList = List.of(task1, task2);
+        taskRepository.saveAll(taskList);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    void testGetAllTasks() {
+    	// When
+        driver.get("http://localhost:" + port + "/tasks");
+        // Then
+        List<WebElement> tasks = driver.findElements(By.className("task-title"));
+        // Assert
+        assertEquals(2, tasks.size());
+        assertEquals("Title1", tasks.get(0).getText());
+        assertEquals("Title2", tasks.get(1).getText());
+    }
+
+    @Test
+    void testShowSaveTaskForm() {
+    	// When
+        driver.get("http://localhost:" + port + "/tasks/create");
+        // Then
+        WebElement formHeader = driver.findElement(By.tagName("h1"));
+        // Assert
+        assertEquals("Create Task", formHeader.getText());
+    }
+
+    @Test
+    void testSaveTask() {
+    	 // When
+        driver.get("http://localhost:" + port + "/tasks/create");
+
+        driver.findElement(By.name("title")).sendKeys("New Task");
+        driver.findElement(By.name("description")).sendKeys("New Description");
+        driver.findElement(By.name("deadline")).sendKeys("2024-12-31");
+        driver.findElement(By.name("status")).sendKeys("Pending");
+
+        Select userSelect = new Select(driver.findElement(By.name("userId")));
+        userSelect.selectByVisibleText("Saad Zafar");
+
+        driver.findElement(By.name("submit")).click();
+        // Then
+        List<Task> tasks = taskRepository.findAll();
+        // Assert
+        assertEquals(3, tasks.size());
+        assertEquals("New Task", tasks.get(2).getTitle());
+    }
+
+    @Test
+    void testShowEditTaskForm() {
+    	// When
+        Task task = taskRepository.findAll().get(0);
+        driver.get("http://localhost:" + port + "/tasks/edit/" + task.getId());
+        // Then
+        WebElement formHeader = driver.findElement(By.tagName("h1"));
+        // Assert
+        assertEquals("Edit Task", formHeader.getText());
+    }
+
+    @Test
+    void testUpdateTask() {
+    	// When
+        Task task = taskRepository.findAll().get(0);
+        driver.get("http://localhost:" + port + "/tasks/edit/" + task.getId());
+        // Then
+        WebElement titleField = driver.findElement(By.name("title"));
+        titleField.clear();
+        titleField.sendKeys("Updated Task");
+        driver.findElement(By.name("submit")).click();
+        Task updatedTask = taskRepository.findById(task.getId()).orElseThrow();
+        // Assert
+        assertEquals("Updated Task", updatedTask.getTitle());
+    }
+
+
+}
---
+++ src/test/java/tms/task_management_system/e2e/UserWebControllerE2E.java
@@ -0,0 +1,118 @@
+package tms.task_management_system.e2e;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import tms.task_management_system.con.Conn;
+import tms.task_management_system.entity.Users;
+import tms.task_management_system.repository.UserRepository;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserWebControllerE2E extends Conn {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	private WebDriver driver;
+
+	@BeforeEach
+	@Transactional
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		driver = new ChromeDriver(options);
+
+		userRepository.deleteAll();
+
+		Users user1 = new Users(1L, "Test User 1", "testuser1@gmail.com", "password1", "ADMIN", null);
+		Users user2 = new Users(2L, "Test User 2", "testuser2@gmail.com", "password2", "USER", null);
+		userRepository.saveAll(List.of(user1, user2));
+	}
+
+	@AfterEach
+	public void tearDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+	@Test
+	void testGetAllUsers() {
+		// When
+		driver.get("http://localhost:" + port + "/users");
+		// Then
+		List<WebElement> users = driver.findElements(By.tagName("tr"));
+		// Assert
+		assertEquals(3, users.size());
+	}
+
+	@Test
+	void testShowSaveUserForm() {
+		// When
+		driver.get("http://localhost:" + port + "/users/create");
+		// Then
+		WebElement formHeader = driver.findElement(By.tagName("h1"));
+		// Assert
+		assertEquals("Create User", formHeader.getText());
+	}
+
+	@Test
+	void testSaveUser() {
+		// When
+		driver.get("http://localhost:" + port + "/users/create");
+
+		driver.findElement(By.id("name")).sendKeys("New User");
+		driver.findElement(By.id("email")).sendKeys("newuser@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("newpassword");
+		driver.findElement(By.id("role")).sendKeys("ADMIN");
+
+		driver.findElement(By.cssSelector("input[type='submit']")).click();
+		// Then
+		List<Users> users = userRepository.findAll();
+		// Assert
+		assertEquals(3, users.size());
+		assertEquals("New User", users.get(2).getName());
+	}
+
+	@Test
+	void testShowEditUserForm() {
+		// When
+		Users user = userRepository.findAll().get(0);
+		driver.get("http://localhost:" + port + "/users/edit/" + user.getId());
+		// Then
+		WebElement formHeader = driver.findElement(By.tagName("h1"));
+		// Assert
+		assertEquals("Edit User", formHeader.getText());
+		assertEquals(user.getName(), driver.findElement(By.id("name")).getAttribute("value"));
+	}
+
+	@Test
+	void testDeleteUserById() {
+		// When
+		Users user = userRepository.findAll().get(0);
+		driver.get("http://localhost:" + port + "/users/delete/" + user.getId());
+		// Then
+		List<Users> users = userRepository.findAll();
+		// Assert
+		assertEquals(1, users.size());
+	}
+}
